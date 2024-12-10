bc85a2a3a0b60c71e5131f7568c5a44b70161be5
CaparicaLeo <leo_caparica@icloud.com>
2024-12-10 03:54:45 -0300

    arrumado

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -33,14 +33,15 @@ const Footer = () => (
         </li>
       </ul>
     </div>
-    <div className='div-imgs'>
+    <div className="div-imgs">
       <p>2024 Music Feel. Todos os direitos reservados.</p>
-      <div>
+      <div className="icons">
         <img src={youtube} alt="YouTube" />
         <img src={instagram} alt="Instagram" />
         <img src={facebook} alt="Facebook" />
       </div>
     </div>
+
   </footer>
 );
 
